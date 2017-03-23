554a2875f091422fe4f7bb2405c4ffb61cc7e74e
Tim Hopper <tdhopper@gmail.com>
2017-03-22 21:19:32 -0400

    get simple_Db working

--- tweet.py
+++ tweet.py
@@ -2,23 +2,50 @@ import schedule
 import time
 import os
 import twitter as tw
-import shelve
 import logging
+import boto3
+import botocore
 
 from random import shuffle
 
-tweet_content = "tweet-content.txt"
-tweet_lookup = "tweet-lookup.shelve"
+
 logging.basicConfig(filename='tweet.log', level=logging.WARNING)
+
+domain = 'newton_tweets' # sdb.create_domain(DomainName="newton_tweets")
+last_seen = 'last_seen'
+pace = 656 # minutes
+
+
+def get_client():
+    return boto3.client('sdb')
 
 
 def initialize():
-    tweets = shelve.open(tweet_lookup)
-    with open(tweet_content, 'r') as f:
+    sdb = get_client()
+    with open('tweet-content.txt', 'r') as f:
         raw_tweets = f.readlines()
-        for tweet in raw_tweets:
-            tweets[tweet.strip()] = 0
-    tweets.close()
+    for tweet in raw_tweets:
+        tweet = tweet.strip()
+        attributes = [
+            {'Name': last_seen, 'Value': str(time.time())}
+        ]
+        expected = {'Name': 'last_seen',
+                    'Exists': False}
+
+        try:
+            sdb.put_attributes(DomainName=domain,
+                               ItemName=tweet,
+                               Attributes=attributes,
+                               Expected=expected)
+        except botocore.exceptions.ClientError as e:
+            logging.info(e)
+
+
+def cleanup_db(c, raw_tweets):
+
+    tweets_in_db = c.select(SelectExpression='select Name from newton_tweets')['Items']
+    tweets_in_db = set(x['Name'] for x in tweets_in_db)
+    # ... need to delete old tweets from db
 
 
 def send_tweet(s):
@@ -34,25 +61,38 @@ def send_tweet(s):
     print("Sent tweet: {}".format(s))
 
 
-def tweet():
-    lookup = shelve.open(tweet_lookup)
-    tweets = list(lookup.keys())
-    tweets = sorted(tweets, key=lambda tweet: lookup[tweet])
+def grab_old_tweet(c):
+    q = "select Name, {} from {}".format(last_seen, domain)
+    tweets = c.select(SelectExpression=q)['Items']
+    tweets = {d['Name']: float(d['Attributes'][0]['Value'])
+              for d in tweets}
+    tweets = sorted(tweets, key=tweets.get) # sort by last tweeted
     tweets = tweets[0:10]
     shuffle(tweets)
     tweet = tweets[0]
-    tweet = tweet.replace(" / ", "\n")
-    lookup[tweet] += 1
+    return tweet.replace(" / ", "\n")
+
+
+def update_db(c, tweet):
+    c.put_attributes(DomainName=domain,
+                     ItemName=tweet,
+                     Attributes=[{'Name': last_seen,
+                                  'Value': str(time.time()),
+                                  'Replace': True}])
+
+
+def tweet():
+    c = get_client()
+    tweet = grab_old_tweet(c)
     send_tweet(tweet)
-    lookup.close()
+    update_db(c, tweet)
 
 
 if __name__ == '__main__':
-    if not os.path.isfile(tweet_lookup + ".db"):
-        initialize()
+    initialize()
 
     tweet()
-    schedule.every(656).minutes.do(tweet)
+    schedule.every(pace).minutes.do(tweet)
 
     while True:
         try:
