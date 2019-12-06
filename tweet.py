e44ee3c87fcc58b0a1b8df672e1c19bc4e80ecac
Tim Hopper <tdhopper@gmail.com>
2019-12-06 14:51:09 -0500

    reformat

--- tweet.py
+++ tweet.py
@@ -8,12 +8,12 @@ from random import shuffle
 
 tweet_content = "tweet-content.txt"
 tweet_lookup = "tweet-lookup.shelve"
-logging.basicConfig(filename='tweet.log', level=logging.WARNING)
+logging.basicConfig(filename="tweet.log", level=logging.WARNING)
 
 
 def get_raw_tweets():
     tweets = []
-    with open(tweet_content, 'r') as f:
+    with open(tweet_content, "r") as f:
         raw_tweets = f.readlines()
         for tweet in raw_tweets:
             tweets.append(tweet.strip())
@@ -22,10 +22,10 @@ def get_raw_tweets():
 
 def send_tweet(s):
     cred = {
-        "consumer_key": os.environ['NEWTON_CONSUMER_KEY'].strip(),
-        "consumer_secret": os.environ['NEWTON_CONSUMER_SECRET'].strip(),
-        "token": os.environ['NEWTON_TOKEN'].strip(),
-        "token_secret": os.environ['NEWTON_TOKEN_SECRET'].strip(),
+        "consumer_key": os.environ["NEWTON_CONSUMER_KEY"].strip(),
+        "consumer_secret": os.environ["NEWTON_CONSUMER_SECRET"].strip(),
+        "token": os.environ["NEWTON_TOKEN"].strip(),
+        "token_secret": os.environ["NEWTON_TOKEN_SECRET"].strip(),
     }
     auth = tw.OAuth(**cred)
     t = tw.Twitter(auth=auth)
@@ -35,9 +35,7 @@ def send_tweet(s):
     print("Sent tweet: {}".format(s))
 
 
-def get_db(bucket="olneyhymnbots",
-           serializer="json",
-           index="content"):
+def get_db(bucket="olneyhymnbots", serializer="json", index="content"):
     db = NoDB()
     db.bucket = bucket
     db.human_readable_indexes
@@ -54,9 +52,8 @@ def tweets_ordered_by_last_sent_time():
         if d is None:
             last_sent[tweet] = 0
         else:
-            last_sent[tweet] = d['last_sent']
-    s = sorted([(time, tweet)
-                for tweet, time in last_sent.items()])
+            last_sent[tweet] = d["last_sent"]
+    s = sorted([(time, tweet) for tweet, time in last_sent.items()])
     return [t for _, t in s]
 
 
@@ -68,10 +65,8 @@ def tweet(a, b):
     send_tweet(tweet)
 
     db = get_db()
-    db.save({"content": tweet,
-             "last_sent": int(time.time()),
-             "from": "newton"})
+    db.save({"content": tweet, "last_sent": int(time.time()), "from": "newton"})
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     pass
